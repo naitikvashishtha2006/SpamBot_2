2f027022d8a1c295ee3581d345b0443f4e612987
codED <naitikvashishtha2006@gmail.com>
2021-06-08 06:43:20 +0530

    Initial commit

---
+++ whatsappCB.py
@@ -0,0 +1,68 @@
+import pyautogui as py
+# import webbrowser
+import time
+# import pymsgbox
+# import os
+# # webbrowser.open("https://www.instagram.com")
+time.sleep(15)
+for i in range(10):
+    # pyautogui.press("Y")
+    # pyautogui.press("o")
+    # pyautogui.press("u")
+    # pyautogui.press(" ")
+    # pyautogui.press("a")
+    # pyautogui.press("r")
+    # pyautogui.press("e")
+    # pyautogui.press(" ")
+    # pyautogui.press("H")
+    # pyautogui.press("a")
+    # pyautogui.press("c")
+    # pyautogui.press("k")
+    # pyautogui.press("e")
+    # pyautogui.press("d")
+    # pyautogui.press("H")
+    # pyautogui.press("e")
+    # pyautogui.press("l")
+    # pyautogui.press("l")
+    # pyautogui.press("o")
+    # pyautogui.press("enter")
+    # pyautogui.moveTo(100,150)
+    # pyautogui.click(100,150)
+    # pyautogui.alert("Help")
+    py.press("Y")
+    py.press("o")
+    py.press("u")
+    py.press(" ")
+    py.press("w")
+    py.press("i")
+    py.press("l")
+    py.press("l")
+    py.press(" ")
+    py.press("H")
+    py.press("i")
+    py.press("t")
+    py.press(" ")
+    py.press("1")
+    py.press(" ")
+    py.press("M")
+    py.press("i")
+    py.press("l")
+    py.press("l")
+    py.press("i")
+    py.press("o")
+    py.press("n")
+    py.press(" ")
+    py.press("s")
+    py.press("o")
+    py.press("o")
+    py.press("n")
+    py.press("enter")
+# import pyautogui
+# distance = 200
+# while distance > 0:
+#     pyautogui.drag(distance, 0, duration=0.5)   # move right
+#     distance -= 5
+#     pyautogui.drag(0, distance, duration=0.5)   # move down
+#     pyautogui.drag(-distance, 0, duration=0.5)  # move left
+#     distance -= 5
+#     pyautogui.drag(0, -distance, duration=0.5)
